b5002a7bf7afe2ee70c9e9fa5d51fccc6333c05f
Jignesh262006 <cjignesh800@gmail.com>
2025-11-06 11:55:15 +0530

    update

--- MyLittleEquipmentTrader/Controllers/CategoryController.cs
+++ MyLittleEquipmentTrader/Controllers/CategoryController.cs
@@ -23,6 +23,7 @@ namespace MyLittleEquipmentTrader.Api.Controllers
         // GET: api/category
         [HttpGet]
         
+        // Neem ka Patta Kadva Hai 
         [Authorize(Policy = "CanViewCategories")]
         public async Task<ActionResult<IEnumerable<CategoryDto>>> GetAllCategories()
         {
